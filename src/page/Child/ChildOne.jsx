096ba6753a3781cc8a40a4709fc875a487048dfb
himaloyash <himaloyashis98@gmail.com>
2023-09-24 23:02:12 +0600

    designed

--- src/page/Child/ChildOne.jsx
+++ src/page/Child/ChildOne.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 const ChildOne = () => {
     return (
-        <div className='max-w-6xl m-10 border-2 border-purple-600  '>
+        <div className='max-w-4xl m-10 border-2 border-purple-600  '>
             <div className=''>
                 <p>1</p>
             </div>
-            <div className='w-full '>
+            <div className='w-full m-auto'>
+                <div className='flex gap-5 justify-center p-10'>
+                    <button className='bg-orange-300 p-2 rounded'>decrease</button>
+                    <p>0</p>
+                    <button className=' bg-lime-300 p-2 rounded '>increase</button>
+
+                </div>
 
             </div>
         </div>
--- src/page/parent/Parent.jsx
+++ src/page/parent/Parent.jsx
@@ -3,7 +3,7 @@ import ChildOne from '../Child/ChildOne';
 
 const Parent = () => {
     return (
-        <div className='border-2 border-orange-700 max-w-7xl mx-auto m-10 rounded-md'>
+        <div className='border-2 border-orange-700 max-w-5xl mx-auto m-10 rounded-md'>
             <h1 className='m-5 text-xl'>Parent</h1>
             <ChildOne></ChildOne>
         </div>
