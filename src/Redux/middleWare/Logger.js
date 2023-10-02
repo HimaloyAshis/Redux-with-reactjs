a7aaeec1ac4374ccc72fa807e30be31e0beadab3
himaloyash <himaloyashis98@gmail.com>
2023-10-02 23:18:43 +0600

    current stated updated

--- src/Redux/middleWare/Logger.js
+++ src/Redux/middleWare/Logger.js
@@ -1,9 +1,14 @@
 const logger = (store)=>(next)=>(action)=>{
 
     const currentState = store.getState()
+    console.log("current",currentState)
+    console.log("action", action)
+    console.log("updated", store.getState())
+    
+    next(action)
 
-    console.log(currentState)
-
+    console.log()
+    
 }
 
 export default logger
