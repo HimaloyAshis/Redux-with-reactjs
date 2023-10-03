a8525edf65cb3a58590fc3fbe1b969c2d17f33e4
himaloyash <himaloyashis98@gmail.com>
2023-10-03 20:46:33 +0600

    all set

--- src/Redux/middleWare/Logger.js
+++ src/Redux/middleWare/Logger.js
@@ -1,14 +1,14 @@
 const logger = (store)=>(next)=>(action)=>{
 
     const currentState = store.getState()
-    
+
     console.log("current",currentState)
     console.log("action", action)
-    console.log("updated", store.getState())
     
     next(action)
+    
+    console.log("updated", store.getState())
 
-    console.log()
     
 }
 
